e7ac2e817ff20ef9e7e63ea309fa0ee18d1baa9a
grtlinux <grtlinux@gmail.com>
2020-08-24 23:12:00 +0900

    ...

--- ver00/KieaSec-00-test/src/org/tain/hmac/HmacMain.java
+++ ver00/KieaSec-00-test/src/org/tain/hmac/HmacMain.java
@@ -11,21 +11,26 @@ public class HmacMain {
 
 	public static void main(String[] args) {
 		try {
-			String key = "12345678901234567890123456789012";
+			// nonce = "yyyyMMddHHmmssSSS";
+			// url = "rest.sentbe.com/getWebviewId";
+			// body = "{'message_key': 'message_value'}";  // json
+			// message = nonce + url + body;
+			
+			String key = "12345678901234567890123456789012";  // Secret-Key
 			String message = "the message to hash here";
 
 			Mac hasher = Mac.getInstance("HmacSHA256");
-			hasher.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
+			hasher.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));   // Secret-Key
 
-			byte[] hash = hasher.doFinal(message.getBytes());
+			byte[] hash = hasher.doFinal(message.getBytes());               // message
 
 			// to lowercase hexits
 			//DatatypeConverter.printHexBinary(hash);
-			System.out.println(">>>>> " + DatatypeConverter.printHexBinary(hash));
+			System.out.println(">>>>> Hexit [" + DatatypeConverter.printHexBinary(hash) + "]");     // Hexit
 
 			// to base64
 			//DatatypeConverter.printBase64Binary(hash);
-			System.out.println(">>>>> " + DatatypeConverter.printBase64Binary(hash));
+			System.out.println(">>>>> Base64 [" + DatatypeConverter.printBase64Binary(hash) + "]");  // Base64
 		}
 		catch (NoSuchAlgorithmException e) {}
 		catch (InvalidKeyException e) {}
